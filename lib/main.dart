77450a9a40d00cb72b31e7de64c86124e8713bd1
nay3li <71359020+nay3li@users.noreply.github.com>
2020-09-24 18:05:13 -0700

    Add picture.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,33 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MyApp() //comment
-    
-  );class MyApp extends StatelessWidget {
-    @override
-    Widget build(BuildContext context) {
-      return MaterialApp(
-      home: Scaffold(
-        backgroundColor: Colors.red
-        body: Container(),
-      ),
-    );
-    }
+  runApp(MyApp() //comment
+      );
+}
+
+class MyApp extends StatelessWidget {
+  get height => null;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      backgroundColor: Colors.teal,
+      body: SafeArea(
+          child: Column(
+        children: <Widget>[
+          CircleAvatar(
+            radius: 50.0,
+            backgroundImage: AssetImage('images/nay.JPG'),
+          ),
+          Text('Nayeli Ansari',
+              style: TextStyle(
+                  fontFamily: 'Pacifico',
+                  fontSize: 40.0,
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold))
+        ],
+      )),
+    ));
   }
 }
