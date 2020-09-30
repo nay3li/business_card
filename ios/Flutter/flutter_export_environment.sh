567af427cbc004bc9e92d9ff0de985563ba76226
nay3li <71359020+nay3li@users.noreply.github.com>
2020-09-30 16:37:53 -0700

    update
    business card update

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/NayeliSantamaria/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/NayeliSantamaria/AndroidStudioProjects/mi_card_flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/NayeliSantamaria/Documents/GitHub/business_card"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
